82ec98c171aa8a83e5d9a207ca92621da6637fc6
kazu0323 <tunakazukiday45@gmail.com>
2021-07-08 12:54:14 +0900

    1

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+     has_many :reviews
+     with_options presence: true do
+      validates :email
+      validates :password,format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: "は、半角英数字混合での入力が必須" }
+        end
+end
