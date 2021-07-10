a0f275c1c2cab3f75f9fb4ef3bff4588a89fe6b5
kazu0323 <tunakazukiday45@gmail.com>
2021-07-11 01:02:22 +0900

    評価の登録機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_141601) do
+ActiveRecord::Schema.define(version: 2021_07_10_154221) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_07_08_141601) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "rate", default: 0.0, null: false
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
