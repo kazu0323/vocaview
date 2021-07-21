13da6dc464faff1ba6e25c49f23e0bdd9856730c
kazu0323 <tunakazukiday45@gmail.com>
2021-07-21 13:49:12 +0900

    Update 20210710154221_add_rate_to_reviews.rb

--- db/migrate/20210710154221_add_rate_to_reviews.rb
+++ db/migrate/20210710154221_add_rate_to_reviews.rb
@@ -1,5 +1,5 @@
 class AddRateToReviews < ActiveRecord::Migration[6.0]
   def change
-    add_column :reviews, :rate, :float, , null: false, default: 0
+    add_column :reviews, :rate, :float, null: false, default: 0
   end
 end
