e2bb262d7a41f72b7eca307722defb1bd841c522
kazu0323 <tunakazukiday45@gmail.com>
2021-07-22 01:16:42 +0900

    表示ミスの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Song.create!(
+  id: 1,
+  song_name: "ヴィラン / flower・てにをは (villain/ flower・teniwoha)"
+  song_image: "https://i.ytimg.com/vi/p9FJXfGHtDA/hqdefault.jpg"
+  song_url: "p9FJXfGHtDA"
+  song_composer: "てにをは"
+)
