367cc7ce1a47889567cbfed24d10b0a0d339aee6
kazu0323 <tunakazukiday45@gmail.com>
2021-07-25 16:21:44 +0900

    検索機能の実施

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation ])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
+
+  def set_search
+    @q = Song.ransack(params[:q])
+    @result = @q.result(distinct: true).limit(12)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "reviews#index"
+  get 'reviews/search'
   resources :users, only: [:show, :edit, :update]
   resources :songs, only: [:show] do
     resources :reviews, only: [:index, :new, :create]
