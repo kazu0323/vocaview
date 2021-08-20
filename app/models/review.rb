16bf691fcca63114edd56bbbed47e098b571779d
kazu0323 <tunakazukiday45@gmail.com>
2021-08-20 23:35:27 +0900

    レビュー文字数上限変更

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
   belongs_to :song
   
   with_options presence: true do
-    validates :review_text, length: { maximum: 75 }
+    validates :review_text, length: { maximum: 280 }
     validates :rate,numericality: {
       less_than_or_equal_to: 5,
       greater_than_or_equal_to: 1}
